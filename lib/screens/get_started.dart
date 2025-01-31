3aeb408daca7bcc960ffbb2a666417402cdea59c
Rayanry9 <rayanhldr@gmail.com>
2025-01-31 19:08:34 +0530

    Feat: Login/Register OAuth Google

--- lib/screens/get_started.dart
+++ lib/screens/get_started.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:uperks/screens/home.dart';
 import 'package:uperks/screens/register.dart';
 import 'package:uperks/widgets/starter_panel.dart';
 
@@ -8,66 +7,65 @@ class GetStarted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: StarterPanel(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Earn the Reward Points you rightfully deserve",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Earn and Spend Reward Points on your day-to-day transactions and BIGG purchases. There's no one in this entire world who will be able to stop you from saving big now!",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  //Navigator.of(context).pushNamed('/');
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Register()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 20),
-                  backgroundColor: Colors.orange,
-                  foregroundColor: Colors.white,
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)),
+        color: Colors.white,
+        child: StarterPanel(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Earn the Reward Points you rightfully deserve",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-                child: Text('Get Started'),
-              ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: () {
-                  print("dabaya");
-                },
-                child: Text('Already have an Account? Login',
-                  style: Theme.of(context).textTheme.bodySmall)
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: GestureDetector(
-                  onTap: () {print("dabaya");},
-                  child: Text('Are you a seller? SignUp here',
-                    style: Theme.of(context).textTheme.bodySmall)
+                SizedBox(height: 10),
+                Text(
+                  "Earn and Spend Reward Points on your day-to-day transactions and BIGG purchases. There's no one in this entire world who will be able to stop you from saving big now!",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium,
                 ),
-              )
-            ],
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    //Navigator.of(context).pushNamed('/');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Register()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 20),
+                    backgroundColor: Colors.orange,
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                  ),
+                  child: Text('Get Started'),
+                ),
+                SizedBox(height: 10),
+                GestureDetector(
+                    onTap: () {
+                      print("dabaya");
+                    },
+                    child: Text('Already have an Account? Login',
+                        style: Theme.of(context).textTheme.bodySmall)),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: GestureDetector(
+                      onTap: () {
+                        print("dabaya");
+                      },
+                      child: Text('Are you a seller? SignUp here',
+                          style: Theme.of(context).textTheme.bodySmall)),
+                )
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,11 +4,9 @@ class Home extends StatelessWidget {
   const Home({super.key});
   @override
   Widget build(BuildContext context) {
-    print(Navigator.of(context).toString());
     return Scaffold(
       body: Text(
         "HomeAAAAAAAAAAAAAAAAA",
-        
         style: Theme.of(context)
             .textTheme
             .bodyMedium!
