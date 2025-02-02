3c76713758a5f4a2f4f46e619949f8bed2319990
Rayanry9 <rayanhldr@gmail.com>
2025-02-02 17:41:41 +0530

    Fix: Payment Screen Seller Name

--- lib/widgets/payment_dialog.dart
+++ lib/widgets/payment_dialog.dart
@@ -14,64 +14,71 @@ class PaymentDialog extends StatelessWidget {
     return Dialog.fullscreen(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       child: SafeArea(
-        
-          
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () => Navigator.pop(context),
-                  ),
-                ),
-                SizedBox(height: 20),
-
-                CircleAvatar(
-                  radius: 40,
-                  backgroundColor: Colors.grey[300],
-                  child: Icon(Icons.person, size: 40, color: Colors.blue),
-                ),
-
-                SizedBox(height: 10),
-                //Text("Requesting ${MyFireBase.instance.sellers.getNameFromId(id)}"),
-                Text(
-                  "Requesting {Seller Name}",
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.black),
-                ),
-                Text(
-                  "Store {Store Name}",
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.grey),
-                ),
-
-                SizedBox(height: 10),
-
-                Text(
-                  "\$500.55", 
-                  style: Theme.of(context).textTheme.displaySmall!.copyWith(color: Colors.black),
-                ),
-
-                SizedBox(height: 350),
-
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(16),
-                        backgroundColor: Colors.blue,
-                      ),
-                      child: Icon(Icons.arrow_forward_ios, color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () => Navigator.pop(context),
+              ),
             ),
-          
-        
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.grey[300],
+              child: Icon(Icons.person, size: 40, color: Colors.blue),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Requesting ${MyFireBase.instance.sellers.getNameFromId(id)}",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.black),
+            ),
+            /*
+            Text(
+              "Requesting {Seller Name}",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.black),
+            ),
+						*/
+            Text(
+              "Store {Store Name}",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "\$500.55",
+              style: Theme.of(context)
+                  .textTheme
+                  .displaySmall!
+                  .copyWith(color: Colors.black),
+            ),
+            SizedBox(height: 350),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(16),
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Icon(Icons.arrow_forward_ios, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
